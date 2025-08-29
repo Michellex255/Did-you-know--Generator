26ffb2b8f410324af5945e380f106221351a0623
Michelle958 <michelle4.dup@gmail.com>
2025-08-29 07:00:59 +0200

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateInfo(event) {
+  event.preventDefault();
 
+  new Typewriter("#info", {
+    strings: [
+      "On Earth's smallest continent, Europe, you'll find the smallest town in the world.",
+    ],
+    autoStart: true,
+    delay: 1,
+    curser: "",
+  });
+}
+
+let infoFormElement = document.querySelector("#info-generator-form");
+infoFormElement.addEventListener("submit", generateInfo);
