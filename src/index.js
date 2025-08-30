f6bfc49274d420a09c4edf8b37bcbd0b5dfbf8ab
Michelle958 <michelle4.dup@gmail.com>
2025-08-30 08:09:19 +0200

    Added the AI API Intergration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-function generateInfo(event) {
-  event.preventDefault();
-
+function displayinfo(response) {
+  console.log("info generated");
   new Typewriter("#info", {
-    strings: [
-      "On Earth's smallest continent, Europe, you'll find the smallest town in the world.",
-    ],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     curser: "",
   });
+}
+
+function generateInfo(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "d374t43e29a36b438a5593oaba8810fc";
+  let context =
+    "You are an expert on funny fun random information about the world and everything in it and love to give short funny random fact. Your mission is to give no more then 3 short lines of funny fun random facts in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the information.";
+  let prompt = `User instructions: Generate short funny fun random info about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating info");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayinfo);
 }
 
 let infoFormElement = document.querySelector("#info-generator-form");
